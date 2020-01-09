0f5e7bf07c1ee7263df40559ed763e1639b59a56
thebestteamaker@gmail.com <thebestteamaker@gmail.com>
2020-01-09 12:15:22 -0800

    first commit

---
+++ precache-manifest.021ce11dbc8b2cc53babcc7961dd8098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3df389c8b3960fd796adfcb5be451cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b45835e23358da8f70d0",
+    "url": "/static/css/main.ce4e3a04.chunk.css"
+  },
+  {
+    "revision": "b9e531fbd91ab63f5786",
+    "url": "/static/js/2.ffaf4656.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ffaf4656.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b45835e23358da8f70d0",
+    "url": "/static/js/main.8840a61b.chunk.js"
+  },
+  {
+    "revision": "0f11a385bedd2fb9a8e6",
+    "url": "/static/js/runtime-main.477caeeb.js"
+  }
+]);
